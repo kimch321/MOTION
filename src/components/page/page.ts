9aba2cfda6f6c4ad4185e838faa2efd379c5fb7f
KIM CHUNG IL <cnddlf12@naver.com>
2023-04-13 18:15:58 +0900

    삭제버튼 구현

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,8 +1,12 @@
 import { BaseComponent, Component } from "../component.js";
 export interface Composable {
     addChild(child: Component): void;
+
 }
+type OnCloseListener = () => void;
+
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+    private closeListner?: OnCloseListener;
     constructor(){
         super(`<li class="page-item">
                 <section class="page-item__body"></section>
@@ -10,11 +14,19 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
                     <button class="close">&times;</button>
                 </div>
             </li>`);
+        const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+        closeBtn.onclick = () => {
+            //this.closeListner가 있다면 호출
+        this.closeListner && this.closeListner();
+        }
     }
     addChild(child: Component) {
         const container = this.element.querySelector('.page-item__body')! as HTMLElement;
         child.attachTo(container);
     };
+    setOnCloseListener(listener: OnCloseListener) {
+        this.closeListner = listener;
+    }
 }
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
     constructor() {
@@ -25,5 +37,8 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
         const item = new PageItemComponent();
         item.addChild(section);
         item.attachTo(this.element, 'beforeend')
+        item.setOnCloseListener(() => {
+            item.removeFrom(this.element)
+        })
     }
 }
