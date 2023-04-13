0beae6bcdc93383cb730793ddb3f478d64035ecd
KIM CHUNG IL <cnddlf12@naver.com>
2023-04-13 17:05:09 +0900

    시작

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import { Component } from "./components/component";
+import { ImageComponent } from "./components/item/image.js";
+import { NoteComponent } from "./components/item/note.js";
+import { TodoComponent } from "./components/item/todo.js";
+import { VideoComponent } from "./components/item/video.js";
+import { Composable, PageComponent } from "./components/page/page.js";
+
+class App{
+    private readonly page: Component & Composable
+    constructor(appRoot: HTMLElement) {
+        this.page = new PageComponent();
+        this.page.attachTo(appRoot);
+
+        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+        this.page.addChild(image)
+        
+        const video = new VideoComponent('Video Title', 'https://youtu.be/K3-jG52XwuQ')
+        this.page.addChild(video)
+        
+        const note = new NoteComponent('Note Title', 'Note Body');
+        this.page.addChild(note)
+
+        const todo = new TodoComponent('Todo Title','Todo item');
+        this.page.addChild(todo)
+    }
+}
+
+new App(document.querySelector('.document')! as HTMLElement)
